0783ac5d2dd361a23d9acff6095b3eedba996d30
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-05-11 16:46:22 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,28 +44,6 @@
                         </div>
                     </div>
                 </div>
-{{--                <div class="col-12">--}}
-{{--                    <div class="banner-odometer">--}}
-{{--                        <div class="counter-item">--}}
-{{--                            <div class="counter-thumb">--}}
-{{--                                <img src="{{ asset('assets/images/icon/counter1.png') }}" alt="icon">--}}
-{{--                            </div>--}}
-{{--                            <div class="counter-content">--}}
-{{--                                <h2 class="title"><span class="counter">17501</span></h2>--}}
-{{--                                <span>Premium User</span>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="counter-item">--}}
-{{--                            <div class="counter-thumb">--}}
-{{--                                <img src="{{ asset('assets/images/icon/counter2.png') }}" alt="icon">--}}
-{{--                            </div>--}}
-{{--                            <div class="counter-content">--}}
-{{--                                <h2 class="title"><span class="counter">1987</span></h2>--}}
-{{--                                <span>Daily Visitors</span>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
             </div>
         </div>
     </section>
@@ -99,20 +77,6 @@
                                     <a href="#0" class="get-button">Select Plan<i class="flaticon-right"></i></a>
                                 </div>
 
-                                <div class="pricing-item-2">
-                                    <h5 class="cate">Free Telegram</h5>
-                                    <div class="thumb">
-                                        <img src="{{ asset('assets/images/pricing/pricing3.png') }}" alt="pricing">
-                                    </div>
-                                    <p>
-                                        Daily 2-3 signals<br><br>
-                                        All day support<br><br>
-                                        Hourly update on each trade<br><br>
-                                        Daily new setups and<br><br>
-                                        news analysis
-                                    </p>
-                                    <a href="#0" class="get-button">Select Plan<i class="flaticon-right"></i></a>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -121,90 +85,6 @@
         </div>
     </section>
     <!--============= Pricing Section Ends Here =============-->
-
-    <!--============= Exclusive Section Starts Here =============-->
-    <section class="exclusive-section padding-bottom-2 padding-top oh" id="feature">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7 col-xl-6">
-                    <div class="section-header left-style">
-                        <h2 class="title">See what You Can Do with PipskomeEA</h2>
-                        <p>
-                            Numerous features make it possible to customize the system in accordance with all your needs.
-                        </p>
-                    </div>
-                    <div class="row mb--20">
-                        <div class="col-sm-6">
-                            <div class="exclusive-item">
-                                <div class="exclusive-thumb">
-                                    <img src="{{ asset('assets/images/feature/01.png') }}" alt="feature">
-                                </div>
-                                <div class="exclusive-content">
-                                    <h6 class="title">Fast Performance</h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="exclusive-item">
-                                <div class="exclusive-thumb">
-                                    <img src="{{ asset('assets/images/feature/02.png') }}" alt="feature">
-                                </div>
-                                <div class="exclusive-content">
-                                    <h6 class="title">Easy Customize</h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="exclusive-item">
-                                <div class="exclusive-thumb">
-                                    <img src="{{ asset('assets/images/feature/03.png') }}" alt="feature">
-                                </div>
-                                <div class="exclusive-content">
-                                    <h6 class="title">Pure & Elegant</h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="exclusive-item">
-                                <div class="exclusive-thumb">
-                                    <img src="{{ asset('assets/images/feature/04.png') }}" alt="feature">
-                                </div>
-                                <div class="exclusive-content">
-                                    <h6 class="title">Secure Data</h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="exclusive-item">
-                                <div class="exclusive-thumb">
-                                    <img src="{{ asset('assets/images/feature/05.png') }}" alt="feature">
-                                </div>
-                                <div class="exclusive-content">
-                                    <h6 class="title">quick access</h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="exclusive-item">
-                                <div class="exclusive-thumb">
-                                    <img src="{{ asset('assets/images/feature/06.png') }}" alt="feature">
-                                </div>
-                                <div class="exclusive-content">
-                                    <h6 class="title">24/7 Support</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5 col-xl-6 d-lg-block d-none">
-                    <div class="feature-1-thumb mt-0 ex-feature">
-                        <img src="{{ asset('assets/images/feature/feature02.png') }}" alt="feature">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--============= Exclusive Section Ends Here =============-->
 
     <!--============= Colaboration Section Starts Here =============-->
     <section class="colaboration-section padding-top-2 padding-bottom-2 oh" id="screenshot">
@@ -306,132 +186,7 @@
             </div>
         </div>
     </section>
-    <!--============= Colaboration Section Ends Here =============-->
-
-    <!--============= Coverage Section Starts Here =============-->
-    <section class="coverage-section padding-top padding-bottom-2" id="coverage">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-7">
-                    <div class="section-header left-style coverage-header">
-                        <h2 class="title">Our Stats</h2>
-                        <p>
-                            Pipskomeea bot users are growing by 300% every year with a steady love from users around the world. We are also close to achieving 45,000 cumulative users.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-lg-5">
-                    <div class="coverage-right-area text-lg-right">
-                        <div class="rating-area">
-                            <div class="ratings">
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                            </div>
-                            <span class="average">5.0 / 5.0</span>
-                        </div>
-                        <h2 class="amount">312,921+</h2>
-                        <a href="#0">Total User Reviews <i class="fas fa-paper-plane"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="coverage-wrapper bg_img" data-background="assets/images/bg/world-map.png">
-                <div class="border-item-1">
-                    <span class="name">North America</span>
-                    <h2 class="title">70.7%</h2>
-                </div>
-                <div class="border-item-2">
-                    <span class="name">Asia</span>
-                    <h2 class="title">14.4%</h2>
-                </div>
-                <div class="border-item-3">
-                    <span class="name">North Europe</span>
-                    <h2 class="title">8.4%</h2>
-                </div>
-                <div class="border-item-4">
-                    <span class="name">South America</span>
-                    <h2 class="title">1.8%</h2>
-                </div>
-                <div class="border-item-5">
-                    <span class="name">Africa</span>
-                    <h2 class="title">1.8%</h2>
-                </div>
-                <div class="border-item-6">
-                    <span class="name">Australia</span>
-                    <h2 class="title">3%</h2>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--============= Coverage Section Ends Here =============-->
-
-
-    <!--============= Testimonial Section Starts Here =============-->
-    <section class="testimonial-section padding-top-2">
-        <div class="container">
-            <div class="section-header">
-                <h5 class="cate">Testimonials</h5>
-                <h2 class="title">5000+ happy clients all around the world</h2>
-            </div>
-            <div class="testimonial-wrapper">
-                <a href="#0" class="testi-next trigger">
-                    <img src="{{ asset('assets/images/client/left.png') }}" alt="client">
-                </a>
-                <a href="#0" class="testi-prev trigger">
-                    <img src="{{ asset('assets/images/client/right.png') }}" alt="client">
-                </a>
-                <div class="testimonial-area testimonial-slider owl-carousel owl-theme">
-                    <div class="testimonial-item">
-                        <div class="testimonial-thumb">
-                            <div class="thumb">
-                                <img src="{{ asset('assets/images/client/client1.jpg') }}" alt="client">
-                            </div>
-                        </div>
-                        <div class="testimonial-content">
-                            <div class="ratings">
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                            </div>
-                            <p>
-                                Awesome product. The guys have put huge effort into this app and focused on simplicity and ease of use.
-                            </p>
-                            <h5 class="title"><a href="#0">Bela Bose</a></h5>
-                        </div>
-                    </div>
-                    <div class="testimonial-item">
-                        <div class="testimonial-thumb">
-                            <div class="thumb">
-                                <img src="{{ asset('assets/images/client/client1.jpg') }}" alt="client">
-                            </div>
-                        </div>
-                        <div class="testimonial-content">
-                            <div class="ratings">
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                                <span><i class="fas fa-star"></i></span>
-                            </div>
-                            <p>
-                                Awesome product. The guys have put huge effort into this app and focused on simplicity and ease of use.
-                            </p>
-                            <h5 class="title"><a href="#0">Raihan Rafuj</a></h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="button-area">
-                    <a href="#0" class="button-2">Leave a review <i class="flaticon-right"></i></a>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--============= Testimonial Section Ends Here =============-->
-
+    <!--============= Collaboration Section Ends Here =============-->
 
     <!--============= Faq Section Starts Here =============-->
     <section class="faq-section padding-top mb-5">
